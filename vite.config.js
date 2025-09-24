d7c832279430046ca86ea82497d1bd762a899e33
Joshua808 <joshua808@gmail.com>
2025-09-23 20:13:08 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/letitride/'   // <-- important for GitHub Pages
+  base: '/letitride'   // <-- important for GitHub Pages
 })
